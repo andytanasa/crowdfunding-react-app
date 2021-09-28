ec0b901d9403cbd1e1764cb6833c2e66e61336d8
andreitanasa8 <andytanasa8@gmail.com>
2021-09-28 10:59:04 +0300

    adding global styles

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  primary: {
+    cyan: "hsl(176, 50%, 47%)",
+    darkCyan: "hsl(176, 72%, 28%)",
+  },
+  neutral: {
+    black: "hsl(0, 0%, 0%)",
+    darkgGray: "hsl(0, 0%, 48%)",
+  },
+};
