d32246eeb82f12d55526a441b8372117ba3fdec0
andreitanasa8 <andytanasa8@gmail.com>
2021-09-28 10:58:26 +0300

    adding global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { GlobalStyles } from "./styles/Global";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
